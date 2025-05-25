915c79de7df6e66c3773e1c8141342c51b97ac46
michee <pawindtaore.nangma-salam@digital.gouv.tg>
2025-05-25 22:20:54 +0000

    feat: implementation des methode création, récuperation et recherche avancée des evenements

--- src/modules/event/controller/admin/event.controller.ts
+++ src/modules/event/controller/admin/event.controller.ts
@@ -1,8 +1,13 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { SearchFilter, StringUtils } from '@app/common/utils';
+import { FilterQuery } from '@app/core/providers/base.mongo.repository';
 import { EventsService } from '@app/events/domain/services/events.service';
+import { Events } from '@app/events/infrastructure/models/events';
+import { EventsRepository } from '@app/events/infrastructure/repositories/events.repository';
 import { JournalService } from '@app/journal/domain/services/journal.service';
 import {
   Controller,
+  Get,
   HttpCode,
   HttpStatus,
   LogLevel,
@@ -16,10 +21,22 @@ import { CreateEventDto } from '../../dto';
 @Admin()
 @Controller('admin/event')
 export class EventController {
+  private readonly searchFilter: SearchFilter;
+
   constructor(
     private readonly eventService: EventsService,
+    private readonly eventsRepository: EventsRepository,
     private readonly journalService: JournalService,
-  ) {}
+  ) {
+    this.searchFilter = new SearchFilter({
+      title: 'string',
+      description: 'string',
+      location: 'string',
+      capacity: 'number',
+      isOnline: 'boolean',
+      tags: 'string',
+    });
+  }
 
   @HttpCode(HttpStatus.CREATED)
   @Post('')
@@ -28,7 +45,61 @@ export class EventController {
       const input = new CreateEventDto(req.body);
 
       const result = await this.eventService.createEvent({ ...input });
+
+      return result;
     } catch (error) {
+      throw error;
+    }
+  }
+
+  @Get()
+  async list(@Req() req: Request) {
+    try {
+      const { limit, skip, query } = req.filterQuery;
+      const filter: FilterQuery<Events> = {};
+
+      if (query) {
+        const numQuery = Number(query);
+        if (StringUtils.isMongoId(query)) {
+          filter._id = query;
+        } else if (!Number.isNaN(numQuery)) {
+          filter.$or = [{ min: numQuery }, { max: numQuery }];
+        } else {
+          const regex = { $regex: query, $options: 'i' };
+          filter.$or = [
+            { title: regex },
+            { description: regex },
+            { location: regex },
+            { tags: regex },
+          ];
+        }
+      }
+
+      filter.deleted = false;
+
+      const events = await this.eventsRepository.get(filter, limit, skip, {
+        name: 1,
+      });
+      return events;
+    } catch (error) {
+      this.log('error', '', error);
+      throw error;
+    }
+  }
+
+  @Post('_search')
+  async search(@Req() req: Request) {
+    try {
+      const { limit, skip } = req.filterQuery;
+      const { filter = {}, sort = {} } = req.body;
+
+      this.searchFilter.handle(filter, sort);
+
+      const events = await this.eventService.get(filter, limit, skip, sort);
+
+      return events;
+    } catch (error) {
+      this.log('error', '', error);
       throw error;
     }
   }
